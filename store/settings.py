5c0ff4dcc7533a4e9a16498455df3d21da1eca98
aargall984 <36456986+aargall984@users.noreply.github.com>
2018-06-25 10:28:16 -0400

    New branch push avoiding conflicts.

--- store/settings.py
+++ store/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'pe$*r=5k1qk#5c4t0uu3$(r%154gbo_b4asuzja0kr$%*5vudu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['138.197.138.180','.islandhomeoutfitters.ca', '127.0.0.1']
+ALLOWED_HOSTS = ['138.197.138.180','.islandhomeoutfitters.ca']
 
 
 # Application definition
